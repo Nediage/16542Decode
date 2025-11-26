2fc8ffc9857d0934318743fb4d26b813144db1aa
Nediage <jaidenrichert@gmail.com>
2025-11-26 12:18:10 -0900

    Added depot starting positions

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/BlueDepot.java
@@ -0,0 +1,34 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class BlueDepot {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-49.8, -49.8, 0.965042))
+                .splineToLinearHeading(new Pose2d(-12, -12, Math.toRadians(225)), Math.toRadians(0))
+                .waitSeconds(5)
+                .strafeToSplineHeading(new Vector2d(11.5, -31), Math.toRadians(270))
+                .waitSeconds(1)
+                .lineToY(-44)
+                .strafeToSplineHeading(new Vector2d(-12, -12), Math.toRadians(225))
+                .waitSeconds(5)
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
